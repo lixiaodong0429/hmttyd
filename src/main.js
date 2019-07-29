0ec99d0626c2d91718b0d7a7359d3e13ab8d9423
李晓东 <5171873+lixdsmayun@user.noreply.gitee.com>
2019-07-29 16:31:53 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Vant,
   render: h => h(App)
 }).$mount('#app')
